9f37ec6a838ae823d9eba6a6383af649b56b7878
Shubh <shubhjhawar78@gmail.com>
2023-08-24 22:09:25 -0400

    fetch and display posts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
 import Head from 'next/head';
+import { PostCard, Categories, PostWidget } from '@/components';
+import { getPosts } from '@/services';
 
-export default function Home() {
+
+export default function Home({posts}) {
     return (
-      <div className="text-3xl font-bold underline">
-        Hello world!
+      <div className="container mx-auto px-20 mb-5">
+        <Head>
+            <title>Echo</title>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+            <div className='col-span-1 lg:col-span-8'>
+                {posts.map((post, index) =><PostCard post={post.node} key={index}/>)}
+            </div>
+            <div className='lg:col-span-4 col-span-1'>
+                <div className='lg:sticky relative top-8'>
+                    <PostWidget />
+                    <Categories />
+                </div>
+            </div>
+        </div>
       </div>
     )
+  }
+
+  //fetching posts
+  export async function getStaticProps() {
+    const posts = (await getPosts()) || [];
+    
+    return {
+        props: { posts }
+    }
   }
