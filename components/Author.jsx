c1d55605f5155d9dfe3c1e912a527c61a425741b
Shubh <shubhjhawar78@gmail.com>
2023-08-26 16:15:03 -0400

    author component implemented

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
-const Author = () => {
+const Author = ({author}) => {
   return (
-    <div>Author</div>
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+      <div className='absolute left-0 right-0 -top-10 flex flex-col items-center'>
+        <Image 
+          alt={author.name}
+          unoptimized
+          height={100}
+          width={100}
+          className='rounded-full'
+          src={author.photo.url}
+        />
+      </div>
+        <h3 className='text-white my-4 text-xl font-bold'>{author.name}</h3>
+        <p className='text-lg'>{author.bio}</p>
+    </div>
   )
 }
 
